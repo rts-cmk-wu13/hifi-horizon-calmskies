ca52a5b23fec365453cb6910ef6e608c1eb29bfd
Neve-06 <6255234@rts-365.dk>
2025-06-13 13:09:02 +0200

    Added image, name and price for the product in Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,14 @@
+import { useLoaderData } from "react-router";
+
 export default function Card() {
+
+        const product = useLoaderData()
 
     return (
         <div>
-            <img src="" alt="" />
-            <p>product name</p>
-            <p>price</p>
+            <img src={product.image} alt="product-image" />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
             <button className="bg-orange-600">Read more</button>
         </div>
     )
