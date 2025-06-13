3a28739f58e6ba3332fcd9378b6a3e3f7abbeb05
Neve-06 <6255234@rts-365.dk>
2025-06-13 10:07:21 +0200

    created Card component. So far its a dummy, more details will be added

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+
+    return (
+        <div>
+            <img src="" alt="" />
+            <p>product name</p>
+            <p>price</p>
+            <button className="bg-orange-600">Read more</button>
+        </div>
+    )
+}
