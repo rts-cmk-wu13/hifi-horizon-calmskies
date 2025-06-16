91887b6e2b86f61ef2637f4d60a1388e175b2ab4
Neve-06 <6255234@rts-365.dk>
2025-06-16 10:50:46 +0200

    Card, Details and List components finished.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card() {
             <img src={product.image} alt="product-image" />
             <p>{product.name}</p>
             <p>{product.price}</p>
-            <button className="bg-orange-600">Read more</button>
+            <button className="bg-orange-600 text-white">Read more</button>
         </div>
     )
 }
